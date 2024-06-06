8d3b551bf405ce9f0f5ea7b458871f57a8ccb877
Sachin Kumar <96615888+sachin-shaw@users.noreply.github.com>
2024-06-06 21:26:08 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://web-elite-versel-1.onrender.com",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
+  plugins: [react()]
 });
