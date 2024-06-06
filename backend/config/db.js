dda3686e7870e08cef8bd74157a5a695e469b88a
Sachin Kumar <96615888+sachin-shaw@users.noreply.github.com>
2024-06-06 16:28:33 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,40 @@
+// import mongoose from "mongoose";
+// import colors from "colors";
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGO_URL);
+//     console.log(
+//       `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
+//     );
+//   } catch (error) {
+//     console.log(`Errro in Mongodb ${error}`.bgRed.white);
+//   }
+// };
+
+// export default connectDB;
+
+
 import mongoose from "mongoose";
 import colors from "colors";
+import dotenv from "dotenv";
+
+// Load environment variables
+dotenv.config();
+
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
+    const conn = await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(
-      `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
+      `Connected To Mongodb Database ${conn.connection.host}`.bgMagenta.white
     );
   } catch (error) {
-    console.log(`Errro in Mongodb ${error}`.bgRed.white);
+    console.log(`Error in Mongodb ${error}`.bgRed.white);
+    process.exit(1); // Exit process with failure
   }
 };
 
 export default connectDB;
+
